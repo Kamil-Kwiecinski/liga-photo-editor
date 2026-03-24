583e964c22d8c18e966b262e3904d734f3983f5f
Kamil Kwieciński <100489738+Kamil-Kwiecinski@users.noreply.github.com>
2026-03-24 22:46:19 +0100

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import PhotoEditor from './PhotoEditor'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PhotoEditor />
+  </React.StrictMode>
+)
